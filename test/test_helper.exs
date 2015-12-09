95cc451197e3147faac5551e46b14699be375137
Ryan Plessner <plessner.ryan@gmail.com>
2015-12-08 20:37:35 -0500

    Fix tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,5 @@
 ExUnit.start
 
-Mix.Task.run "ecto.create", ["--quiet"]
-Mix.Task.run "ecto.migrate", ["--quiet"]
+Mix.Task.run "ecto.create", ["--quiet", "-r", "PhoenixDdd.Persistence"]
+Mix.Task.run "ecto.migrate", ["--quiet", "-r", "PhoenixDdd.Persistence"]
 Ecto.Adapters.SQL.begin_test_transaction(PhoenixDdd.Persistence)
